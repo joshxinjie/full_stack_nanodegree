9f3a9964a9251792ddc938dfc281df14a3d36576
joshxinjie <leexinjie@gmail.com>
2021-10-24 18:01:19 +0800

    reorganisation

---
+++ lessons/4_Identity_and_Access_Management/2_identity_and_authentication/14_practise_sending_tokens/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, abort
+from functools import wraps
+
+app = Flask(__name__)
+
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+    
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+    
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+        
+    return header_parts[1]
+
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header()
+        return f(jwt, *args, **kwargs)
+    return wrapper
+
+@app.route('/headers')
+@requires_auth
+def headers(jwt):
+    # @TODO unpack the request header
+    print(jwt)
+    return jwt
+
